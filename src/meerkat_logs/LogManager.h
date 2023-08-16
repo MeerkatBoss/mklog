728ffa4d691d06174f275bdc3882f6bfc37b196b
MeerkatBoss <ivan.solodovnikov@outlook.com>
2023-08-16 10:21:23 +0300

    feat: implement LogWriter management in LogManager

--- src/meerkat_logs/LogManager.h
+++ src/meerkat_logs/LogManager.h
@@ -18,9 +18,72 @@
 namespace meerkat_logs
 {
 
+/**
+ * @brief Main class used for logging. Manages all LogWriters. Accepts and
+ * distributes log messages.
+ */
 class LogManager
 {
+private:
+  /**
+   * @brief Linked list node containing LogWriter
+   */
+  struct WriterListNode
+  {
+    LogWriter*      writer;
+    WriterListNode* next;
+  };
+
+  /**
+   * @brief Head of linked list of LogWriter
+   */
+  static WriterListNode* s_writerListHead;
+
+  /**
+   * @brief State of LogManager
+   */
+  enum class Status
+  {
+    UNINITIALIZED,
+    READY,
+    DEINITIALIZED,
+  };
+
+  /**
+   * @brief Current state of LogManager
+   */
+  static Status s_currentStatus;
+
+  /**
+   * @brief End all logging. Invalidate LogManager
+   */
+  static void endLogs();
+
 public:
+  /**
+   * @brief Register new LogWriter in LogManager
+   *
+   * @tparam TWriter  LogWriter implementation
+   *
+   * @param[in] ...	  TWriter constructor arguments
+   */
+  template <typename TWriter, typename... TArgs>
+  static void addWriter(TArgs... args)
+  {
+    s_writerListHead = new WriterListNode{.writer = new TWriter(args...),
+                                          .next   = s_writerListHead};
+  }
+
+  /**
+   * @brief Initialize logging for program
+   */
+  static void initLogs();
+
+  /**
+   * @brief Send log message to all registered writers
+   *
+   * @param[in] message	  Log message to be sent
+   */
   static void logMessage(const LogMessage& message);
 };
 
