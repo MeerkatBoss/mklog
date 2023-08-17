a616f6ed21be79e6b5f2c4a3b8748ac11912d580
MeerkatBoss <ivan.solodovnikov@outlook.com>
2023-08-17 19:05:16 +0300

    chore: update example to include long messages

--- src/main.cpp
+++ src/main.cpp
@@ -13,32 +13,70 @@
   do                                                                           \
   {                                                                            \
     if (!(condition))                                                          \
-      dprintf(fd, "Failed assertion '%s': %s\n\tat %s:%z in function '%s'\n",  \
+    {                                                                          \
+      dprintf(fd, "Failed assertion '%s': %s\n\tat %s:%d in function '%s'\n",  \
               #condition, message, __FILE__, __LINE__, __PRETTY_FUNCTION__);   \
+      abort();                                                                 \
+    }                                                                          \
   } while (0)
 
 void dummyHandler(int) { puts("Interrupt handled in main.cpp"); }
 
+void setupLogs()
+{
+  using meerkat_logs::LogManager;
+  using meerkat_logs::LogRoute;
+  using meerkat_logs::MessageSeverity;
+
+  LogManager::addWriter<meerkat_logs::StderrLogWriter>(
+      LogRoute::makeRoute<meerkat_logs::SeverityRoutingRule>(
+          /* minLevel = */ MessageSeverity::INFO),
+      /* useEscapeCodes = */ true);
+  LogManager::initLogs();
+}
+
 int main()
 {
   using meerkat_logs::Logger;
-  using meerkat_logs::LogManager;
-  using meerkat_logs::LogRoute;
   using meerkat_logs::MessageContentType;
-  using meerkat_logs::MessageSeverity;
+  using LogMessageFd = meerkat_logs::LogManager::MessageFd;
 
   sigset(SIGINT, &dummyHandler);
-  LogManager::addWriter<meerkat_logs::StderrLogWriter>(
-      LogRoute::makeRoute<meerkat_logs::SeverityRoutingRule>(
-          MessageSeverity::MIN_LEVEL),
-      /* useEscapeCodes = */ true);
-  LogManager::initLogs();
+  setupLogs();
+
   Logger logger("main");
 
   logger.LOG_INFO(MessageContentType::TEXT, "Started logs");
+  logger.LOG_TRACE(MessageContentType::TEXT, "Entered main()");
+  LogMessageFd longMsgFd =
+      logger.LOG_BEGIN_INFO(MessageContentType::TEXT, "This is a long message");
+
+  dprintf(longMsgFd, "This is some content\n");
 
   raise(SIGINT);
+
+  dprintf(longMsgFd, "This is some more content\n");
+  dprintf(longMsgFd, "Long message ends here\n");
+
+  logger.endLongMessage(longMsgFd);
+  LogMessageFd emptyMsgFd =
+      logger.LOG_BEGIN_FATAL(MessageContentType::TEXT, "This won't be printed");
+
+  fassert(emptyMsgFd, 2 + 2 == 4, "This is clearly true");
+
   raise(SIGINT);
+
+  fassert(emptyMsgFd, 2 * 2 == 4, "This is also true");
+
+  logger.endLongMessage(emptyMsgFd);
+
+  LogMessageFd assertFd =
+      logger.LOG_BEGIN_FATAL(MessageContentType::TEXT, "VALIDATION ERROR");
+
+  fassert(assertFd, 1 < 2, "True assertion");
+  fassert(assertFd, 2 < 1, "False assertion");
+
+  logger.endLongMessage(assertFd);
 
   return 0;
 }
